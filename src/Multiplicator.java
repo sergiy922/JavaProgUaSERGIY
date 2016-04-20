da4204c1140e926a759405a451ae52b46ec2132a
Sergiy Moroz <Sergiy Moroz>
2016-04-20 21:13:53 +0300

    Initial commit

---
+++ src/Multiplicator.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Ira on 20.04.2016.
+ */
+public class Multiplicator {
+    public static int mul(int x,int y) {
+        int accumulator = 0;
+        for (int i = 0; i < y; i++) {
+            accumulator = add(accumulator, x);
+        }
+        return accumulator;
+    }
+    public static int add(int x,int y){
+        return x+y;
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Ira on 20.04.2016.
+ */
+public class Runner {
+    public static void main(String[] args){
+        int a=4;
+        int b=2;
+
+        int mul=Multiplicator.mul(a,b);
+       // Multiplicator.add();
+        System.out.println(mul);
+    }
+}
